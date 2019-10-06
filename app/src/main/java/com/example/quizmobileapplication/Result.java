132a9d6cf0a98f302c69d7ba46391e4a441e3d2a
Emilien <emilien.pons@viacesi.fr>
2019-10-06 17:06:27 +0800

    Add Results Table

--- app/src/main/java/com/example/quizmobileapplication/Result.java
+++ app/src/main/java/com/example/quizmobileapplication/Result.java
@@ -1,4 +1,12 @@
 package com.example.quizmobileapplication;
 
+import java.util.Date;
+
 public class Result {
+
+    private int IDQuiz;
+    private int IDUser;
+    private Date date;
+    private int score;
+
 }

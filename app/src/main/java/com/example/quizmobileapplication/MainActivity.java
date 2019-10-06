8ce08037e571b8f10196a77505701e9181f45ed5
Emilien <emilien.pons@viacesi.fr>
2019-10-06 14:25:33 +0800

    Test d'initialisation sous Android studio.

--- app/src/main/java/com/example/quizmobileapplication/MainActivity.java
+++ app/src/main/java/com/example/quizmobileapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //Test initialisation Git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
